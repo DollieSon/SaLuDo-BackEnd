f9dbd99e57b9b8c424f97ca0aeb624f7afa15c33
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2025-05-03 00:25:48 +0800

    added mongo_db access

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  hobbies: [String]
+})
+
+const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
   res.send('Hello from Render!');
@@ -11,6 +26,16 @@ app.get('/', (req, res) => {
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Here is your data.' });
+});
+
+app.get('/api/users', (req, res) => {
+  try{
+   const users =  User.find({});
+   res.json(users);
+  }catch(err){
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.listen(PORT, () => {
